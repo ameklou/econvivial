a19d13e8de268f184292a4e7ead844f982b4691a
Eklou Amemassovor <amekley@gmail.com>
2018-05-28 16:35:36 +0000

    assistance home

--- econvivial/urls.py
+++ econvivial/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     url(r'^main/', include('main.urls')),
     url(r'^account/', include('account.urls')),
     url(r'^consultation/', include('consultation.urls')),
+    url(r'^assistance/', include('assistance.urls')),
 ]+ static(settings.MEDIA_URL,
                          document_root=settings.MEDIA_ROOT)
 
