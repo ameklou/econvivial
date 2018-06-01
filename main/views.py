f07f20899c7ecd55dd64215fc2798f9c12911c79
Eklou Amemassovor <amekley@gmail.com>
2018-06-01 09:10:50 +0000

    recent posts add to home

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from conseil.models import Post
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    posts = Post.published.all()[:3]
+    return render(request, 'main/index.html', {'posts':posts})
